035806b7f4a9a72f6207e64f2835ee065186257f
wilfredlopez <cubamc@gmail.com>
2019-08-18 13:08:01 -0400

    inicial refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { createHashHistory } from 'history'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Main from './main'
+import * as serviceWorker from './serviceWorker'
+import configureStore from './configureStore'
+import './css/index.scss'
+import './css/App.scss'
+
+const history = createHashHistory()
+
+const initialState = window.initialReduxState
+const store = configureStore(history, initialState)
+
+ReactDOM.render(<Main store={store} history={history} />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister()
